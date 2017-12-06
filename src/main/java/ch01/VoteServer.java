f7ded8cab0df89216d8edc9b0e407438d293157a
MelloYuki <352983198@qq.com>
2017-12-06 22:15:37 +0800

    update

--- src/main/java/ch01/VoteServer.java
+++ src/main/java/ch01/VoteServer.java
@@ -13,11 +13,17 @@ import java.util.*;
  */
 public class VoteServer {
     private static final int ONE_WEEK_IN_SECONDS = 7 * 86400;
+    /*
+        ONE_WEEK_IN_SECONDS/200  假设200为文章能被展示的基本票数
+     */
     private static final int VOTE_SCORE = 432;
+    /*
+
+     */
     private static final int ARTICLES_PER_PAGE = 25;
 
     public static void main(String[] args) {
-
+        new VoteServer().run();
     }
 
     public void run() {
@@ -27,6 +33,7 @@ public class VoteServer {
         String articleId = postArticle(conn, "username", "A title", "www.google.com");
         System.out.println("posted a new article with id: " + articleId);
         System.out.println("hash looks like: ");
+        //获取hash表数据
         Map<String, String> articleData = conn.hgetAll("article:" + articleId);
         for (Map.Entry<String, String> entry : articleData.entrySet()) {
             System.out.println("  " + entry.getKey() + ": " + entry.getValue());
@@ -43,6 +50,7 @@ public class VoteServer {
         printArticles(articles);
         assert articles.size() >= 1;
 
+        //对文章进行分组
         addGroups(conn, articleId, new String[]{"new-group"});
         System.out.println("We added the article to a new group, other articles include:");
         articles = getGroupArticles(conn, "new-group", 1);
@@ -50,13 +58,23 @@ public class VoteServer {
         assert articles.size() >= 1;
     }
 
+    /**
+     * @param conn  当前连接
+     * @param user  用户名
+     * @param title 标题
+     * @param link  链接
+     * @return
+     */
     public String postArticle(Jedis conn, String user, String title, String link) {
+        //增长id
         String articleId = String.valueOf(conn.incr("article:"));
 
         String voted = "voted:" + articleId;
+        // voted:id 某篇文章的唯一标识 自动将发布者添加到文章已投票用户名单
         conn.sadd(voted, user);
+        //过期时间设置 一周
         conn.expire(voted, ONE_WEEK_IN_SECONDS);
-
+        // now 时间戳 s
         long now = System.currentTimeMillis() / 1000;
         String article = "article:" + articleId;
         HashMap<String, String> articleData = new HashMap<>();
@@ -65,13 +83,22 @@ public class VoteServer {
         articleData.put("user", user);
         articleData.put("now", String.valueOf(now));
         articleData.put("votes", "1");
+        //设置多个键值对 存储在散列表中
         conn.hmset(article, articleData);
+        //设置排序集合 根据评分排序与根据发布时间排序
         conn.zadd("score:", now + VOTE_SCORE, article);
         conn.zadd("time:", now, article);
 
         return articleId;
     }
 
+    /**
+     * 为文章进行投票
+     *
+     * @param conn    连接
+     * @param user    用户名
+     * @param article 唯一文章鉴定
+     */
     public void articleVote(Jedis conn, String user, String article) {
         long cutoff = (System.currentTimeMillis() / 1000) - ONE_WEEK_IN_SECONDS;
         if (conn.zscore("time:", article) < cutoff) {
@@ -79,10 +106,13 @@ public class VoteServer {
         }
 
         String articleId = article.substring(article.indexOf(':') + 1);
+        //该id首次对某文章进行投票 则进行加分值操作
         if (conn.sadd("voted:" + articleId, user) == 1) {
+            //分值自增
             conn.zincrby("score:", VOTE_SCORE, article);
-            conn.hincrBy(article, "votes", 1l);
-        }
+            //更新投票数量
+            conn.hincrBy(article, "votes", 1);
+        } //这三个操作理论上应该进行事务控制
     }
 
 
@@ -105,6 +135,12 @@ public class VoteServer {
         return articles;
     }
 
+    /**
+     * 将文章进入指定分组
+     * @param conn 连接
+     * @param articleId 文章id
+     * @param toAdd 分组
+     */
     public void addGroups(Jedis conn, String articleId, String[] toAdd) {
         String article = "article:" + articleId;
         for (String group : toAdd) {
@@ -112,10 +148,25 @@ public class VoteServer {
         }
     }
 
+    /**
+     * 获取分组文章
+     * @param conn 连接
+     * @param group 分类的群组
+     * @param page 页数
+     * @return 分组列表
+     */
     public List<Map<String, String>> getGroupArticles(Jedis conn, String group, int page) {
         return getGroupArticles(conn, group, page, "score:");
     }
 
+    /**
+     *
+     * @param conn 连接
+     * @param group 分类的群组
+     * @param page 页数
+     * @param order 排序群组key
+     * @return
+     */
     public List<Map<String, String>> getGroupArticles(Jedis conn, String group, int page, String order) {
         String key = order + group;
         if (!conn.exists(key)) {
@@ -126,6 +177,10 @@ public class VoteServer {
         return getArticles(conn, page, key);
     }
 
+    /**
+     * 打印文章列表
+     * @param articles
+     */
     private void printArticles(List<Map<String, String>> articles) {
         for (Map<String, String> article : articles) {
             System.out.println("  id: " + article.get("id"));
